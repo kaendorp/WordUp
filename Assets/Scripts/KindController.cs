d38b32fcd04a714bc7a507ed7244c91fe9a81a93
PDvelzen <peter.velzen@gmail.com>
2015-04-12 22:26:18 +0200

    Nogmaals

--- Assets/Scripts/KindController.cs
+++ Assets/Scripts/KindController.cs
@@ -8,7 +8,10 @@ public class KindController : MonoBehaviour
 
     // Bericht
     public string message = "";         // Het bericht wat getoont moet worden, gebruik \n om een nieuwe regel te beginnen
-    public GameObject messageObject;    // TextMesh
+    public GameObject messageObject;    // TextMesh    
+
+    // Kind gevonden?
+    private bool gevonden;
 
     // Use this for initialization
     void Start()
@@ -16,6 +19,8 @@ public class KindController : MonoBehaviour
         // Normaal gezien is een bericht een enkele regel
         // hiermee wordt een newline ge-escaped
         message = message.Replace("\\n", "\n");
+
+        gevonden = false;
     }
 
     // Update is called once per frame
@@ -27,7 +32,31 @@ public class KindController : MonoBehaviour
     {
         if (collision.gameObject.tag == targetLayer)
         {
-            messageObject.GetComponent<TextMesh>().text = message;
+            messageObject.GetComponent<TextMesh>().text = message;            
+
+            // Als kind nog niet is gevonden
+            if (gevonden == false)
+            {
+                Debug.Log("FOUND");
+                gevonden = true;
+
+                Player.kindPlus = true;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == targetLayer)
+        {            
+            // Als kind nog niet is gevonden
+            if (gevonden == false)
+            {
+                Debug.Log("FOUND");
+                gevonden = true;
+
+                Player.kindPlus = true;
+            }
         }
     }
 
