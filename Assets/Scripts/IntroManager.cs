bba4bc3e2635184467ebabcc18dc92bb09db34ac
Jochem Klingeler <jochem.klingeler@gmail.com>
2015-04-19 21:34:21 +0200

    Tweaked timing, commentary

--- Assets/Scripts/IntroManager.cs
+++ Assets/Scripts/IntroManager.cs
@@ -2,7 +2,14 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class IntroManager : MonoBehaviour {
+public class IntroManager : MonoBehaviour
+{
+
+    private enum IntroState
+    {
+        evilGloating,
+        bossMoveOut
+    }
 
     public GameObject boss;
     private Animator bossAnim;
@@ -17,13 +24,12 @@ public class IntroManager : MonoBehaviour {
     private float minXValue; // minimal X position of healthbar
     private float maxXValue; // maximal X position of healthbar
 
-    private IntroState _state = IntroState.woordenZijnZwak;
+    private IntroState _state = IntroState.evilGloating;
 
-    private bool waitCoroutineStarted = false;
-    private bool waitOver = false;
+    private bool waitCoroutineStarted = false;      // Prevents the coroutine to trigger multiple times in FixedUpdate()
 
     private float step;
-    private float speed = 1;
+    private float speed = 0.5f;             // Speed of Silence walking away
     private bool bossMoveOut = false;
     private Vector3 targetposition;
 
@@ -31,26 +37,18 @@ public class IntroManager : MonoBehaviour {
     public string message = "";             // The message the fiendly will display when the player gets close
     public GameObject messageObject;        // TextMesh object that will display our message
 
-    private enum IntroState
-    {
-        woordenZijnZwak,
-        niemandKanJeHelpen,
-        bossVertrekt,
-        kindRed
-    }
-
+    // Fade in/out
     public float fadeInSpeed = 1.5f;        // Speed that the screen fades from black.
     public float fadeOutSpeed = 1.5f;       // Speed that the screen fades to black.
     private bool sceneStarting = true;      // Whether or not the scene is still fading in.
-    public Image overlay;
-    
-    void Awake ()
-    {
-        StartCoroutine(FadeToClear());
-    }
+    public Image overlay;                   // Object in HUD that fills screen with a full alpha, black image
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
+        /**
+         * Taken from the hudscript, hardcoded for use in intro
+         */
         cachedY = healthTransform.position.y;
         maxXValue = healthTransform.position.x;
         minXValue = healthTransform.position.x - healthTransform.rect.width;
@@ -61,55 +59,55 @@ public class IntroManager : MonoBehaviour {
 
         visualHealth.color = new Color32(255, (byte)MapValues(currentHealth, 0, maxHealth / 2, 0, 255), 0, 255);
 
-
+        // Get animators and trigger the player 'hit' animation
         bossAnim = boss.GetComponent<Animator>();
         playerAnim = player.GetComponent<Animator>();
         playerAnim.SetBool("IntroKnockback", true);
 
+        // set the location where the Silence will walk to
         targetposition = new Vector3(boss.transform.position.x + 5, boss.transform.position.y, boss.transform.position.z);
-	}
 
+        // Start the intro with a fade to clear
+        StartCoroutine(FadeToClear());
+    }
+
+    /**
+     * Needed for hud
+     */
     private float MapValues(float x, float inMin, float inMax, float outMin, float outMax)
     {
         return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
     }
 
-	// Update is called once per frame
-	void FixedUpdate () {
+    // Update is called once per frame
+    void FixedUpdate()
+    {
         if (bossMoveOut)
             step = speed * Time.deltaTime;
         switch (_state)
         {
-            case IntroState.woordenZijnZwak:
+            case IntroState.evilGloating:
                 if (!waitCoroutineStarted)
-                    StartCoroutine(WoordenZijnZwak());
+                    StartCoroutine(EvilGloating());
                 break;
-            case IntroState.bossVertrekt:
+            case IntroState.bossMoveOut:
                 BossMoveOut();
                 break;
-            case IntroState.kindRed:
-                break;
         }
-	
-	}
 
-    IEnumerator Wait(int wait)
-    {
-        waitCoroutineStarted = true;
-        waitOver = false;
-        yield return new WaitForSeconds(wait);
-        waitOver = true;
-        waitCoroutineStarted = false;
     }
 
-    IEnumerator WoordenZijnZwak()
+    IEnumerator EvilGloating()
     {
         waitCoroutineStarted = true;
         yield return new WaitForSeconds(5);
         bossAnim.SetTrigger("IntroBossRoarIdle");
+        yield return new WaitForSeconds(1);
         messageObject.GetComponent<TextMesh>().text = "Je woorden zijn zwak,\nniets wat je zegt\nkan mij raken!";
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(4);
+        messageObject.GetComponent<TextMesh>().text = "";
         bossAnim.SetTrigger("IntroBossRoarIdle");
+        yield return new WaitForSeconds(1);
         messageObject.GetComponent<TextMesh>().text = "Niemand hier kan je helpen,\nhahaha!";
         yield return new WaitForSeconds(5);
         messageObject.GetComponent<TextMesh>().text = "";
@@ -119,9 +117,11 @@ public class IntroManager : MonoBehaviour {
         boss.transform.localScale = theScale;
         yield return new WaitForSeconds(1);
         bossMoveOut = true;
-        _state = IntroState.bossVertrekt;
+        _state = IntroState.bossMoveOut;
         StartCoroutine(FadeToBlack());
         yield return new WaitForSeconds(10);
+
+        // Load the tutorial level
         Application.LoadLevel("Tutorial");
         waitCoroutineStarted = false;
     }
@@ -133,7 +133,6 @@ public class IntroManager : MonoBehaviour {
         if (boss.transform.position == targetposition)
         {
             bossMoveOut = false;
-            _state = IntroState.kindRed;
         }
     }
 
@@ -154,8 +153,6 @@ public class IntroManager : MonoBehaviour {
 
     IEnumerator FadeToBlack()
     {
-        //overlay.color = Color.Lerp(overlay.color, Color.black, fadeSpeed * Time.deltaTime);
-
         // Lerp the colour of the texture between itself and transparent.
         float rate = fadeOutSpeed;
         float progress = 0f;
