0a99477a77bfa59ca1d261590fc254998b40a018
Jochem Klingeler <jochem.klingeler@gmail.com>
2015-04-20 00:09:13 +0200

    SpawnParticles selfdestruct

---
+++ Assets/Scripts/ParticleSelfDestruct.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSelfDestruct : MonoBehaviour {
+    public float lifeTime = 2f;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SelfDestruct());	
+	}
+	
+    IEnumerator SelfDestruct()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        if (this.gameObject != null)
+            Destroy(this.gameObject);
+    }
+}
