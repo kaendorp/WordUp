7a6c490c63fb02d5d883ec1556c2ecc5b4ba3e27
Yozine <michelle.haanstra@gmail.com>
2015-04-20 20:36:25 +0200

    Green door is open
    -Can displace to green room (without key)
    -Moved scenes into Level3.1

---
+++ Assets/Scripts/Deur.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnitySampleAssets.CrossPlatformInput;
+
+public class Deur : MonoBehaviour {
+
+	// Use this for initialization
+	public static bool open = true;
+	public GameObject spawnPoint;
+	public Camera mainCamera;
+
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerStay2D(Collider2D collision)
+	{
+		if (collision.gameObject.tag == "Player") 
+		{
+			if(CrossPlatformInputManager.GetButtonDown ("Jump"))
+			{
+
+				mainCamera.transform.position = new Vector3(spawnPoint.transform.position.x, spawnPoint.transform.position.y, spawnPoint.transform.position.z);
+				collision.transform.position = spawnPoint.transform.position;
+			}
+		}
+	}
+}
