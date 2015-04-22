dc835ad97d9e6817e1829d4ec2d424120b07c094
PDvelzen <peter.velzen@gmail.com>
2015-04-22 23:47:21 +0200

    Level3 aanpassingen
    Lampen en platformen verschijnen als de speler op een knop staat

---
+++ Assets/Scripts/KnopScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KnopScript : MonoBehaviour {
+
+    public GameObject knop;
+    public GameObject licht;
+
+    public GameObject platform6;
+    public GameObject platform7;
+
+	// Use this for initialization
+	void Start () 
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {        
+        if (collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(ButtonPress());
+            
+            // Zet licht aan
+            licht.GetComponent<Light>().enabled = true;
+            
+            // Activeer platformen            
+            platform6.SetActive(true);
+            platform7.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(ButtonPress());            
+        }        
+    }
+
+    IEnumerator ButtonPress()
+    {
+        knop.GetComponent<Animator>().enabled = true;
+        yield return new WaitForSeconds(1);
+        knop.GetComponent<Animator>().enabled = false;
+    }
+}
