8c3f44beac1442eee36e4d157f16a0dfc557c1b4
PDvelzen <peter.velzen@gmail.com>
2015-04-22 23:54:05 +0200

    Knop animatie aanpassing

--- Assets/Scripts/KnopScript.cs
+++ Assets/Scripts/KnopScript.cs
@@ -9,10 +9,12 @@ public class KnopScript : MonoBehaviour {
     public GameObject platform6;
     public GameObject platform7;
 
+    private bool ingedrukt;
+
 	// Use this for initialization
 	void Start () 
     {
-        
+        ingedrukt = false;
 	}
 	
 	// Update is called once per frame
@@ -25,24 +27,29 @@ public class KnopScript : MonoBehaviour {
     {        
         if (collision.gameObject.tag == "Player")
         {
-            StartCoroutine(ButtonPress());
+            if (ingedrukt == false)
+            {
+                StartCoroutine(ButtonPress());
             
-            // Zet licht aan
-            licht.GetComponent<Light>().enabled = true;
+                // Zet licht aan
+                licht.GetComponent<Light>().enabled = true;
             
-            // Activeer platformen            
-            platform6.SetActive(true);
-            platform7.SetActive(true);
+                // Activeer platformen            
+                platform6.SetActive(true);
+                platform7.SetActive(true);
+
+                ingedrukt = true;
+            }            
         }
     }
 
-    void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            StartCoroutine(ButtonPress());            
-        }        
-    }
+    //void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        StartCoroutine(ButtonPress());            
+    //    }        
+    //}
 
     IEnumerator ButtonPress()
     {
