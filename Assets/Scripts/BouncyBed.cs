63b92b698bf2d1a22cd48ea34a70e7e1277876db
Yozine <michelle.haanstra@gmail.com>
2015-04-24 20:56:55 +0200

    Bouncy bed
    -Collider ladder aangepast 3.3
    -Sleutel script aangepast
    -BouncyBed script toegevoegd
    -Level 3.2 aangepast, raam hoger, extra platform

---
+++ Assets/Scripts/BouncyBed.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BouncyBed : MonoBehaviour {
+
+	GameObject bouncer;
+	public Vector2 velocity;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionStay2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Player") 
+		{
+			bouncer = collision.gameObject;
+			bouncer.GetComponent<Rigidbody2D>().velocity = velocity;
+		}
+	}
+}
--- Assets/Scripts/Sleutel.cs
+++ Assets/Scripts/Sleutel.cs
@@ -4,8 +4,6 @@ using System.Collections;
 public class Sleutel : MonoBehaviour {
 
 	public GameObject deur;
-
-	private bool _triggered;
 	//private AudioSource _audioSource;
 	// Use this for initialization
 	void Awake () 
@@ -15,18 +13,15 @@ public class Sleutel : MonoBehaviour {
 	
 	void Update () 
 	{
-		if (_triggered ) //&& !_audioSource.isPlaying
-		{
-			Destroy (gameObject);
-			deur.SendMessage("GetKey");
 
-		}
 	}
 
 	void OnTriggerEnter2D(Collider2D collision)
 	{
-		if (collision.gameObject.tag == "Player") {
-			_triggered = true;
+		if (collision.gameObject.tag == "Player") 
+		{
+			Destroy (gameObject);
+			deur.SendMessage("GetKey");
 		}
 
 		//_audioSource.enabled = true;
