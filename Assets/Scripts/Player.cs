b5d8115a83f5d96e1a19738541f4375996b7cc8d
PDvelzen <peter.velzen@gmail.com>
2015-05-02 00:50:55 +0200

    The Sage
    Sage NPC gemaakt voor het triggeren van de wordgame.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -48,7 +48,7 @@ public class Player : MonoBehaviour {
 
 	private RectTransform letterPanel;
 
-	private int countLetters;
+	public int countLetters;
 	private int CountLetters // Sets current amount of kids through HandleKids()
 	{
 		get { return countLetters;}
@@ -319,11 +319,7 @@ public class Player : MonoBehaviour {
 				letterPanel.gameObject.SetActive(false);
 				Destroy(collision.gameObject);				
                 HUD.GetComponent<WordGameScript>().Active = true;
-			}
-			else
-			{
-				// To do: Block Player
-			}
+			}			
 		}
 		
 		if (collision.gameObject.tag == "Finish") 
