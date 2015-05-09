c84ad383456fb2e4290cbabdce7eed96033fe3aa
ajmkaandorp <ajmkaandorp@hotmail.com>
2015-05-09 16:56:50 +0200

    Get Achievements, Post Achievements

---
+++ Assets/FBAchievement.cs
@@ -0,0 +1,172 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Facebook;
+using UnityEngine.UI;
+using Facebook.MiniJSON;
+using System.Globalization;
+using System.Text;
+
+public class FBAchievement : MonoBehaviour
+{
+    //Achievement URL (moet meegegeven worden bij zowel GIVE als GET methoden, toString() is belangrijk!
+
+    //Voor TestScene
+    private string btnText = "Give achievement";
+    public GameObject UIFB_IsLoggedIn;
+    public GameObject UIFB_IsNotLoggedIn;
+    private List<object> resultList = null;
+    private List<object> dataList = null;
+    void Awake()
+    {
+        FB.Init(SetInit, OnHideUnity);
+    }
+
+    //Bottom of code (last method), aangeroepen, (eerst ervoor zorgen dat facebook geinitialized is en de user dus ingelogged is);
+    void AchievementCalls()
+    {
+        Debug.Log("Start");
+        //Op deze manier moeten de classes worden aangeroepen:
+        GiveOneAchievement("http://wordupgame.tk/Facebook/Html/Achievements/A_Warmte.html".ToString());
+        //Wait a few seconds before calling the GET if wished to instantiate right after eachother
+        GetOneAchievement(951155231582815, "http://wordupgame.tk/Facebook/Html/Achievements/A_Warmte.html".ToString());
+        GetAllAchievements();
+        GetAllAppAchievements();
+    }
+
+    //ACHIEVEMENT METHODS (Player):
+
+    public void GiveOneAchievement(string achievementUrl)
+    {
+        var dict = new Dictionary<string, string>();
+        Debug.Log(achievementUrl);
+        dict["achievement"] = achievementUrl;
+        FB.API(FB.UserId + "/achievements", Facebook.HttpMethod.POST, null, dict);
+    }
+
+    void GetOneAchievement(long achievementId, string achievementUrl)
+    {
+        var dict = new Dictionary<string, string>() { { "achievement", achievementUrl } };
+        Debug.Log(achievementId + " " + achievementUrl);
+        FB.API(FB.UserId + "/achievements/" + achievementId, Facebook.HttpMethod.GET, HandleGetAchievement, dict);
+    }
+
+    void GetAllAchievements()
+    {
+        FB.API(FB.UserId + "/achievements", Facebook.HttpMethod.GET, HandleGetAchievement);
+    }
+
+    //ACHIEVEMENT METHOD APP
+    void GetAllAppAchievements()
+    {
+        FB.API(FB.AppId + "/achievements", Facebook.HttpMethod.GET, HandleGetAllAppAchievements);
+    }
+
+    //HANDLE METHODS
+    void HandleGetAchievement(FBResult result)
+    {
+        if (result != null)
+        {
+            dataList = Util.DeserializeScores(result.Text);
+            foreach (object dataInstance in dataList)
+            {
+                var entry = (Dictionary<string, object>)dataInstance;
+                var data = (Dictionary<string, object>)entry["data"];
+
+                var achievements = (Dictionary<string, object>)data["achievement"];
+
+                Debug.Log(achievements["id"].ToString());
+                Debug.Log(achievements["title"].ToString());
+            }
+        }
+    }
+
+    void HandleGetAllAppAchievements(FBResult result)
+    {
+        if (result != null)
+        {
+            dataList = Util.DeserializeScores(result.Text);
+            foreach(object dataInstance in dataList)
+            {
+                var entry = (Dictionary<string, object>)dataInstance;
+
+                Debug.Log(entry["id"].ToString());
+                Debug.Log(entry["title"].ToString());
+                Debug.Log(entry["description"].ToString());
+
+                List<object> images = entry["image"] as List<object>;
+                foreach (object image in images)
+                {
+                    var imageEntry = (Dictionary<string, object>)image;
+                    Debug.Log(imageEntry["url"].ToString());
+                }
+
+             
+
+            }
+        }
+
+    }
+
+    //METHODS FOR TESTSCENE
+    private void SetInit()
+    {
+        Debug.Log("FB Initialized");
+
+        if (FB.IsLoggedIn)
+        {
+            Debug.Log("FB logged in");
+            HandleFBMenus(true);
+        }
+        else
+        {
+            HandleFBMenus(false);
+        }
+    }
+
+    private void OnHideUnity(bool isGameShown)
+    {
+        if (!isGameShown)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    public void FBLogin()
+    {
+        FB.Login("email, publish_actions", AuthCallBack);
+    }
+
+    void HandleFBMenus(bool isLoggedIn)
+    {
+        if (isLoggedIn)
+        {
+            UIFB_IsLoggedIn.SetActive(true);
+            UIFB_IsNotLoggedIn.SetActive(false);
+
+        }
+        else
+        {
+            UIFB_IsLoggedIn.SetActive(false);
+            UIFB_IsNotLoggedIn.SetActive(true);
+        }
+    }
+
+
+    void AuthCallBack(FBResult result)
+    {
+        if (FB.IsLoggedIn)
+        {
+            Debug.Log("FB logged in");
+            AchievementCalls();
+        }
+        else
+        {
+            Debug.Log("FB login failed");
+        }
+    }
+}
