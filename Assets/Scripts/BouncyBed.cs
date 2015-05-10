7bb58eb332dde0132d052df27dd4be7b8f8f0cf4
Jochem Klingeler <jochem.klingeler@gmail.com>
2015-05-10 20:12:13 +0200

    BouncyBed, only pick root object of player

--- Assets/Scripts/BouncyBed.cs
+++ Assets/Scripts/BouncyBed.cs
@@ -13,10 +13,6 @@ public class BouncyBed : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-		player = GameObject.Find ("Player");
-		if (player == null) {
-			player = GameObject.Find ("Player2");
-		}
 		playSound = false;
 		_audioSource = gameObject.GetComponent<AudioSource>().clip;
 		positie = gameObject.transform.position;
@@ -27,14 +23,22 @@ public class BouncyBed : MonoBehaviour {
 		
 	}
 
-	void OnTriggerEnter2D(Collider2D collision)
-	{
-		if (collision.gameObject.tag == "Player") 
-		{
-			playSound = true;
-			player.GetComponent<Rigidbody2D>().velocity = velocity;
-		}
-	}
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            player = collision.gameObject;
+            // Make sure we send the parent object
+            // root never returns null, if this Transform doesn't have a parent it returns itself.
+            if (player.transform.root.gameObject != player)
+            {
+                player = player.transform.root.gameObject;
+            }
+
+            playSound = true;
+            player.GetComponent<Rigidbody2D>().velocity = velocity;
+        }
+    }
 
 	void OnTriggerExit2D(Collider2D collision)
 	{
