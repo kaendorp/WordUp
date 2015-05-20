54f83eac9261acadbeab0f2c97ee3f1a1e8f73f0
Jochem Klingeler <jochem.klingeler@gmail.com>
2015-05-20 16:26:43 +0200

    EnemyProjectile only play hitsound on not player
    Already plays hitsound (pain)

--- Assets/Scripts/EnemyProjectileController.cs
+++ Assets/Scripts/EnemyProjectileController.cs
@@ -20,12 +20,6 @@ public class EnemyProjectileController : MonoBehaviour {
 
     void onTriggerEnter2D(Collision2D collided)
     {
-        //If it collides with anything, destroy projectile, if the object dies too fast, error, that's why, check
-        if (this.gameObject)
-        {
-            AudioSource.PlayClipAtPoint(projectileHits, positie, 0.5f);
-        }
-
         //If collides with player
         if (collided.gameObject.tag == "Player" || collided.gameObject.tag == "Friendly")
         {
@@ -33,18 +27,13 @@ public class EnemyProjectileController : MonoBehaviour {
         }
         else
         {
+            AudioSource.PlayClipAtPoint(projectileHits, positie, 0.5f);
             Destroy(this.gameObject);
         }
     }
 
     void OnCollisionEnter2D(Collision2D collided)
     {
-        //If it collides with anything, destroy projectile, if the object dies too fast, error, that's why, check
-        if (this.gameObject)
-        {
-            AudioSource.PlayClipAtPoint(projectileHits, positie, 0.5f);
-        }
-
         //If collides with player
         if (collided.gameObject.tag == "Player" || collided.gameObject.tag == "Friendly")
         {
@@ -52,6 +41,7 @@ public class EnemyProjectileController : MonoBehaviour {
         }
         else
         {
+            AudioSource.PlayClipAtPoint(projectileHits, positie, 0.5f);
             Destroy(this.gameObject);
         }
     }
