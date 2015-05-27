649d3724e8ff76c1482101722ad7bb1d37152a1d
Jochem Klingeler <jochem.klingeler@gmail.com>
2015-05-27 15:11:15 +0200

    Moved AnalyticsCheckpoint to Sleutel & Checkpoint

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Cloud.Analytics;
 
 public class CheckPoint : MonoBehaviour {
 
     public GameMaster gameMaster;
+    private bool analyticsDone = false;
+    public int checkpointNumber = 0;
 
     // Use this for initialization
     void Start()
@@ -22,6 +26,31 @@ public class CheckPoint : MonoBehaviour {
         {
 			gameObject.GetComponent<Animator> ().SetBool ("checked", true);
             gameMaster.currentCheckPoint = gameObject;
+
+            if (!analyticsDone && checkpointNumber != 0)
+                StartGameAnalytics();
         }
+    }
+
+    void StartGameAnalytics()
+    {
+        string customEventName = "Checkpoint" + checkpointNumber + Application.loadedLevelName;
+
+        UnityAnalytics.CustomEvent(customEventName, new Dictionary<string, object>
+        {
+            { "runningTime", Time.timeSinceLevelLoad },
+            { "damageTaken", GameControl.control.damageTaken },
+            { "projectile1Shot", GameControl.control.projectile1Shot },
+            { "projectile2Shot", GameControl.control.projectile2Shot },
+            { "projectile3Shot", GameControl.control.projectile3Shot },
+            { "kidsFound", GameControl.control.kidsFound },
+            { "lettersFound", GameControl.control.lettersFound },
+            { "enemyDefeated", GameControl.control.enemiesDefeated },
+            { "respawns", GameControl.control.respawns },
+            // { "timesPaused", GameControl.control.timesPaused },
+            { "pauseDuration", GameControl.control.pauseDuration },
+        });
+        analyticsDone = true;
+        Debug.Log(customEventName + ": Done");
     }
 }
