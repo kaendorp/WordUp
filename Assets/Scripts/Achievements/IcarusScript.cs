afd64bbf017fb4c1331a5f62d53d13d2b10cd0e8
PDvelzen <peter.velzen@gmail.com>
2015-06-01 21:17:29 +0200

    Level1 Icarus Achievement

---
+++ Assets/Scripts/Achievements/IcarusScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IcarusScript : MonoBehaviour 
+{
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameControl.control.icarusFall = true;
+        }
+    }
+}
