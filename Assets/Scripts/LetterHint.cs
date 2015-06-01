893e90370ce92bc86ad5b60812f76ca875637940
Yozine <michelle.haanstra@gmail.com>
2015-06-01 17:50:35 +0200

    Play Sound on Letter proximity
    -Play sound
    -Changed the letter prefab

---
+++ Assets/Scripts/LetterHint.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LetterHint : MonoBehaviour 
+{
+	private AudioClip _audioSource;
+	private Vector3 positie;
+	// Use this for initialization
+	void Start () 
+	{
+		_audioSource = gameObject.GetComponent<AudioSource>().clip;
+		positie = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collision)
+	{        
+		if (collision.gameObject.tag == "Player")
+		{
+			AudioSource.PlayClipAtPoint (_audioSource, positie, 0.1f);
+		}
+	}
+}
